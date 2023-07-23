4f3d2aaaf4bbfba755c9c6834869bf2039c01695
daniil ushkan <ushkandn@gmail.com>
2023-07-23 23:31:28 +0300

    database connect

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,20 @@ import express from 'express'
 import { connect } from 'mongoose'
 
 const app = express()
+const PORT = process.env.PORT || 7856
+const MONGODB = process.env.MONGODB_URL
+
 app.use(express.json())
 
-const MONGODB = process.env.MONGODB_URL
 connect(MONGODB)
-  .then(() => console.log('mongoDB connected!'))
+  .then(() => {
+    console.log('mongoDB connected!')
+    app.listen(PORT, () => {
+      try {
+        console.log(`Server started on port - ${PORT}`)
+      } catch (err) {
+        console.log('Server error! - ', err)
+      }
+    })
+  })
   .catch((err) => console.log('mongoDB error!', err))
-
-const PORT = process.env.PORT || 7856
-app.listen(PORT, () => {
-  try {
-    console.log(`Server started on port - ${PORT}`)
-  } catch (err) {
-    console.log('Server error! - ', err)
-  }
-})
